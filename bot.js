5112234f09a0f78950609fe6b4bc5c5ea68d0a35
mikeyxgcg <45147548+mikeyxgcg@users.noreply.github.com>
2019-01-22 22:37:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1574,7 +1574,7 @@ if(!message.channel.guild) return message.reply(' ');
 
   if (!rank) return message.channel.send('🛑 **| يجب ان تمتلك رتبة [Js Owners] لأستخدام هذا الأمر.**');
 
-  let jscodes = message.guild.channels.find(`name`, "✽-discord-js");
+  let jscodes = message.guild.channels.find(`name`, "discord-js");
 
   if(!jscodes) return message.channel.send("❌لم اجد الروم الخاص بنشر الاكواد");
 
